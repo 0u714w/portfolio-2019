b3b75272d2b4ebccbc3413a39e4e0a5153ca6f64
Doug Enas <douglassenas@gmail.com>
2019-01-25 13:09:43 -0500

    m

---
+++ build/precache-manifest.058051ae899cdfd87d1743af6ff64795.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb370d3a0ea6520c7df1",
+    "url": "/portfolio/static/css/main.b07abcaa.chunk.css"
+  },
+  {
+    "revision": "bb370d3a0ea6520c7df1",
+    "url": "/portfolio/static/js/main.bb370d3a.chunk.js"
+  },
+  {
+    "revision": "2dd542f619be9275cae3",
+    "url": "/portfolio/static/js/1.2dd542f6.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "9cec4535d6c7f539902210c853000f6c",
+    "url": "/portfolio/static/media/art.9cec4535.svg"
+  },
+  {
+    "revision": "a57a717e26e7ff6ae88c90e349aee6f0",
+    "url": "/portfolio/static/media/profile.a57a717e.jpg"
+  },
+  {
+    "revision": "a8ff30f9b95ce1fc4ecf1f9d79fe913c",
+    "url": "/portfolio/static/media/kwitter.a8ff30f9.png"
+  },
+  {
+    "revision": "f1a7f6fd3d15f376cc02d9665cb3a541",
+    "url": "/portfolio/static/media/birdnerd.f1a7f6fd.png"
+  },
+  {
+    "revision": "f0ec01a944abca3cf7857059c6395f78",
+    "url": "/portfolio/static/media/sokoban.f0ec01a9.png"
+  },
+  {
+    "revision": "c2f04d244a017f25988bb5eb1a9238f6",
+    "url": "/portfolio/static/media/connectfour.c2f04d24.png"
+  },
+  {
+    "revision": "f475a74a3e98a004a68df893377e32e7",
+    "url": "/portfolio/static/media/rockpaperscissors.f475a74a.png"
+  },
+  {
+    "revision": "6a2641e014bf124fe99d594ec30de021",
+    "url": "/portfolio/index.html"
+  }
+];
